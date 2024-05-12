16606075e023102a0e84d0362d542535ade2b4c5
Abdou Ngom <abdoungom0093@gmail.com>
2024-05-12 01:09:20 -0400

    myhello.py

--- hello.py
+++ hello.py
@@ -2,7 +2,10 @@ def hello_world():
     # Remove the # in the line below and delete the word pass
     # print("Hello World!")
 
-    pass
+    def hello():
+        print("Hello World!")
+        hello()
+        
    
 # Do not modify the code below this line
 if __name__ == "__main__":
